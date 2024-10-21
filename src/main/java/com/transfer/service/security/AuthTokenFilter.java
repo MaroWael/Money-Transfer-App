4d3af00993815f00c1b6849592b1348136e9472e
manci ayman <manciayman0@gmail.com>
2024-10-21 23:34:15 +0300

    remove unused code

--- src/main/java/com/transfer/service/security/AuthTokenFilter.java
+++ src/main/java/com/transfer/service/security/AuthTokenFilter.java
@@ -27,7 +27,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenBlacklist tokenBlacklist;
-
+    
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
