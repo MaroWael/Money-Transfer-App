2514d9f5e162ce723375daba3ae404bcc1f5e50b
manci ayman <manciayman0@gmail.com>
2024-11-30 15:27:35 +0200

    remove generated id by default

--- src/main/java/com/transfer/entity/Account.java
+++ src/main/java/com/transfer/entity/Account.java
@@ -21,7 +21,6 @@ import java.time.LocalDateTime;
 public class Account {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
